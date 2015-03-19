577e156383310bd4801914e0009ca675596d7474
prasanna ramanujam <prasannaraajkumar@gmail.com>
2015-03-19 02:39:29 -0700

    client added with bootstrap ui

---
+++ client/js/app.js
@@ -0,0 +1,100 @@
+//$(window).load(function() {
+$(document).ready(function() {
+    console.log('page ready');
+    var hash = window.location.hash;
+    if (hash === '#home') {
+        showHome();
+    } else if (hash === '#signup') {
+        showSignup();
+    } else if (hash === "#login") {
+        showLogin();
+    } else {
+        showLogin();
+    }
+
+    function showSignup() {
+        window.location.hash = 'signup';
+        $(".login").hide();
+        $(".home").hide();
+        $("#btn-signout").hide();
+        $(".signup").show();
+    }
+
+    function showHome() {
+        window.location.hash = 'home';
+        $.ajax({
+            method: 'GET',
+            url: 'api/user?emailid=' + 'p@gmail.com'
+        }).done(function(data) {
+            console.log("get user success");
+            $(".signup").hide();
+            $(".login").hide();
+            $("#btn-signout").show();
+            $(".home").show();
+
+        }).fail(function(data) {
+            showLogin();
+        });
+
+    }
+
+    function showLogin() {
+    	window.location.hash = '#';
+        $(".home").hide();
+        $(".signup").hide();
+        $("#btn-signout").hide();
+        $(".login").show();
+    }
+    
+    $('#btn-signout').click(function() {
+        $.ajax({
+            method: 'GET',
+            url: 'api/logout'
+        }).done(function(data) {
+            console.log("signout success");
+            showLogin();
+        });
+    });
+
+    $('#btnSignin').click(function() {
+        var dataObj = {
+            emailid: $('#loginEmail').val(),
+            password: $('#loginPassword').val()
+        };
+        $.ajax({
+            method: 'POST',
+            url: 'api/login',
+            contentType: 'application/json',
+            data: JSON.stringify(dataObj),
+            dataType: 'json'
+        }).done(function(data) {
+            console.log("login success");
+            showHome();
+        }).fail(function() {
+            alert("login failed !");
+        })
+    });
+
+    $('#btn-signup').click(function() {
+        showSignup();
+    });
+
+    $('#btn-submit').click(function() {
+        var dataObj = {
+            "emailid": $('#inputEmail').val(),
+            "password": $('#inputPassword').val(),
+            "name": $('#inputName').val(),
+            "city": $('#inputCity').val()
+        };
+        $.ajax({
+            method: 'POST',
+            url: 'api/user',
+            contentType: 'application/json',
+            data: JSON.stringify(dataObj),
+            dataType: 'json'
+        }).done(function(data) {
+            console.log('sign up success');
+            showHome();
+        });
+    });
+});
